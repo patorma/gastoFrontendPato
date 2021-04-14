d62d40e44a759a0f631299106ec05fc272dbc75d
patorma <pocontreras@ing.ucsc.cl>
2021-04-13 23:33:26 -0400

    creando el servicio de auth y su metodo login()

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from '../components/usuarios/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario: Usuario):Observable<any> {
+    const urlEndPoint = 'http://localhost:8080/oauth/token';
+
+    // credenciales del cliente angular, hay que encriptarlas
+    //en js se hace con btoa()
+    const credenciales = btoa('angularapp' + ':' + '12345');
+
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded','Authorization': 'Basic' + credenciales});
+    let params = new URLSearchParams();
+    params.set('grant_type','password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+    
+     return this.http.post<any>(urlEndPoint,params,{headers: httpHeaders});
+  }
+}
