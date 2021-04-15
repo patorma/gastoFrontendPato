2ef8bc48af04df3c65e763a728ae0194b78b334f
patorma <pocontreras@ing.ucsc.cl>
2021-04-15 02:04:16 -0400

    se agrego la funcionalidad de cerrar la sesion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'App Angular';
-  constructor() {}
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    let username = this.authService.usuario.username
+    this.authService.logout();
+    swal.fire('Logout',`Hola ${username}, has cerrado sesión con éxito!`,'success');
+    this.router.navigate(['/login']);
+    
+  }
 
   ngOnInit(): void {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -92,4 +92,12 @@ export class AuthService {
     return false;
   }
 
+  logout():void{
+    this._token = null;
+    this._usuario = null;
+    sessionStorage.clear();
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('usuario');
+  }
+
 }
