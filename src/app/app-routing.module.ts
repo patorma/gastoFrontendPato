c27ebb3ebf67780caa49e67a9ecae1730c7cd3a0
patorma <pocontreras@ing.ucsc.cl>
2021-04-21 19:36:30 -0400

    se creo un guard para las rutas y controlar el acceso a recursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { FormComponent } from './components/gastos/form.component';
 import { NotasComponent } from './components/notas/notas.component';
@@ -7,18 +7,19 @@ import { FormNotaComponent } from './components/notas/form-nota.component';
 import { NoEncotradoComponent } from './components/no-encotrado/no-encotrado.component';
 import { FiltroComponent } from './components/filtro/filtro.component';
 import { LoginComponent } from './components/usuarios/login.component';
+import { AuthGuard } from './components/usuarios/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/gastos', pathMatch: 'full' },
   { path: 'gastos', component: GastosComponent },
   { path: 'gastos/page/:page', component: GastosComponent },
-  {path: 'gastos/form', component: FormComponent},
-  {path: 'gastos/form/:id', component: FormComponent},
+  {path: 'gastos/form', component: FormComponent, canActivate: [AuthGuard]},
+  {path: 'gastos/form/:id', component: FormComponent, canActivate: [AuthGuard]},
   {path: 'notas', component: NotasComponent},
   {path: 'notas/page/:page', component: NotasComponent},
-  {path: 'notas/form', component: FormNotaComponent},
-  {path: 'notas/form/:id', component: FormNotaComponent},
-  {path: 'gastos/busquedaGasto', component: FiltroComponent},
+  {path: 'notas/form', component: FormNotaComponent, canActivate: [AuthGuard]},
+  {path: 'notas/form/:id', component: FormNotaComponent, canActivate: [AuthGuard]},
+  {path: 'gastos/busquedaGasto', component: FiltroComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: '**', component: NoEncotradoComponent}
 
